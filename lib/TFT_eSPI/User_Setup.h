713fd92bc55fb17b7b970adef6e0e6855c872fc8
solawc <1225093964@qq.com>
2023-05-16 03:28:02 +0800

    set freq at 80MHz

--- lib/TFT_eSPI/User_Setup.h
+++ lib/TFT_eSPI/User_Setup.h
@@ -342,8 +342,8 @@
 // #define SPI_FREQUENCY  27000000
 // #define SPI_FREQUENCY  40000000
 // #define SPI_FREQUENCY  55000000 // STM32 SPI1 only (SPI2 maximum is 27MHz)
-// #define SPI_FREQUENCY  80000000
-#define SPI_FREQUENCY  65000000
+#define SPI_FREQUENCY  80000000
+// #define SPI_FREQUENCY  65000000
 
 // Optional reduced SPI frequency for reading TFT
 #define SPI_READ_FREQUENCY  20000000
